304df5cc707795ecc842826fd172e12048d176d5
ncr00t <geritix@mail.ru>
2018-07-04 21:13:43 +0400

    create commands for bot

---
+++ commands.js
@@ -0,0 +1,17 @@
+var greetings = ["Hello", "Hi", 
+                 "Privet", "Zdarova",
+                 "Zdarov", "Привет",
+                 "Здарова","Здаров"];
+var shedule = ["Расписание","Пары на сегодня",
+			   "Пары","Пары на завтра"];
+var declaration = ["Заявление на матпомощь", "Матпомощь","Мат помощь",
+                   "Материальная помощь","Заявление на материалку",
+                   "Заявление на материальную помощь"];
+var listCommand = ["Расписание/Пары", "Матпомощь/Заявление на материальную помощь", "ИОС"];
+
+module.exports = {
+    greetings: greetings,
+    shedule : shedule,
+    declaration : declaration,
+    listCommand : listCommand
+};
